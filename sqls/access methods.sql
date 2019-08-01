585e6d2933f8c0451c627619a887214387511b9b
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2019-08-01 20:43:03 +0300

    Update slides and demo SQLs

--- sqls/access methods.sql
+++ sqls/access methods.sql
@@ -1,16 +1,31 @@
+
+------ Scanning techniques ------
 DROP INDEX IF EXISTS t_a_idx;
 
 -- Sequential scan
-EXPLAIN SELECT * FROM t WHERE a = 5;
+EXPLAIN(costs off) SELECT * FROM t WHERE a = 5;
 
 -- Add index
 CREATE INDEX t_a_idx ON t(a);
 
 -- Index scan
-EXPLAIN SELECT * FROM t WHERE a = 5;
+EXPLAIN(costs off) SELECT * FROM t WHERE a = 5;
 
 -- Bitmap scan
-EXPLAIN SELECT * FROM t WHERE a <= 100;
+EXPLAIN(costs off) SELECT * FROM t WHERE a <= 100;
 
 -- Index only-scan
-EXPLAIN SELECT a FROM t WHERE a = 5;
+EXPLAIN(costs off) SELECT a FROM t WHERE a = 5;
+
+------ Partial indexes ------
+CREATE INDEX IF NOT EXISTS t_c_idx ON t(c);
+EXPLAIN(costs off) SELECT * FROM t WHERE c;
+EXPLAIN(costs off) SELECT * FROM t WHERE NOT c;
+-- Check number of pages
+SELECT relpages FROM pg_class WHERE relname='t_c_idx';
+
+DROP INDEX IF EXISTS t_c_idx;
+CREATE INDEX IF NOT EXISTS t_c_idx ON t(c) WHERE c;
+
+
+
--- sqls/setup.sql
+++ sqls/setup.sql
@@ -1,11 +1,11 @@
 BEGIN;
-DROP TABLE t;
+DROP TABLE IF EXISTS t;
 COMMIT;
 
 BEGIN;
 create table t(a integer, b text, c boolean);
 insert into t(a,b,c)
   select s.id, chr((32+random()*94)::integer), random() < 0.01
-  from generate_series(1,100000) as s(id)
+  from generate_series(1,1000000) as s(id)
 order by chr;
 COMMIT;
